7a149b10333790ea68ffc3748556c52c9469bc7a
Jahongir <bright19.off@gmail.com>
2026-02-18 22:40:46 +0500

    Fixed database connection and cloudinary settings

--- core/settings.py
+++ core/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
