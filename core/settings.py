0da2f9f41ed46661fdfcc76abdbc9b1572c9ec97
Jahongir <bright19.off@gmail.com>
2026-02-23 20:43:41 +0500

    fix debug production

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,9 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
@@ -115,7 +117,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 
-import os
+
 import dj_database_url
 
 DATABASES = {
